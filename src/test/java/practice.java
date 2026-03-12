7ddd182f968c7da6fa1e303645c9e876f065195b
Debanjan <debanjandhoshdastidar444@gmail.com>
2026-03-12 17:01:31 +0530

    12th March 2026 commit

--- src/test/java/practice.java
+++ src/test/java/practice.java
@@ -17,7 +17,7 @@ public class practice {
         driver.get("https://int.zigzag.lk/collections/work-wear-1/products/side-pleated-long-sleeve-brown-satin-top");
         
         driver.manage().window().maximize();
-        driver.manage().deleteAllCookies();
+       
         String expectedText = driver.findElement(By.xpath("//span[contains(text(),'Note: Product Colour May Slightly Vary Due To Phot')]")).getText();
 						
         
